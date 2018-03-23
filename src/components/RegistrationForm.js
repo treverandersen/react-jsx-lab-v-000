31f3480832bd13f26cb6fce77daca622cf23e281
Trever Andersen <trever.r.andersen@gmail.com>
2018-03-23 15:11:24 +0000

    automatically backed up by learn

---
+++ src/components/RegistrationForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class RegistrationForm extends React.component {
+  render() {
+    return (
+      <form>
+        <input type="text"></input>
+        <input type="password"></input>
+        <button type="submit"></button> 
+      </form>
+    );
+  }
+}
---
+++ src/components/Webpage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Webpage extends React.component {
+  render() {
+    return (
+      <h1>"The world's coolest webpage"</h1>
+      <FillerText />
+      <FillerText />
+    );
+  }
+}
